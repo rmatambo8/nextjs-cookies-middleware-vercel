05415d74b19c351f758725bddab10fa4ffd6d89e
Rodney Matambo <rmatambo@wistia.com>
2025-01-13 12:25:10 -0500

    testing

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,40 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
 export function middleware(request: NextRequest) {
   // Assume a "Cookie:nextjs=fast" header to be present on the incoming request
   // Getting cookies from the request using the `RequestCookies` API
-  let cookie = request.cookies.get('nextjs')
-  console.log(cookie) // => { name: 'nextjs', value: 'fast', Path: '/' }
-  const allCookies = request.cookies.getAll()
-  console.log(allCookies) // => [{ name: 'nextjs', value: 'fast' }]
- 
-  request.cookies.has('nextjs') // => true
-  request.cookies.delete('nextjs')
-  request.cookies.has('nextjs') // => false
- 
+  let cookie = request.cookies.get("nextjs");
+  console.log("cookie", cookie); // => { name: 'nextjs', value: 'fast', Path: '/' }
+  const allCookies = request.cookies.getAll();
+  console.log("all cookies", allCookies); // => [{ name: 'nextjs', value: 'fast' }]
+
+  console.log("has: ", request.cookies.has("nextjs")); // => true
+  console.log("delete entry:", request.cookies.delete("nextjs"));
+  console.log("has: ", request.cookies.has("nextjs")); // => false
+
   // Setting cookies on the response using the `ResponseCookies` API
-  const response = NextResponse.next()
-  if (!request.cookies.has('vercel')) {
-    response.cookies.set('vercel', 'fast')
+  const response = NextResponse.next();
+  const existingCookies = response.cookies.getAll();
+  console.log("existing cookies: ", existingCookies); // => undefined
+  if (!request.cookies.has("vercel")) {
+    console.log("setting cookie");
+    response.cookies.set("vercel", "fast");
     response.cookies.set({
-      name: 'vercel',
-      value: 'fast',
-      path: '/',
-    })
-    cookie = response.cookies.get('vercel')
-    console.log(cookie) // => { name: 'vercel', value: 'fast', Path: '/' }
+      name: "vercel",
+      value: "fast",
+      path: "/",
+    });
+
+    cookie = response.cookies.get("vercel");
+    console.log("outgoing cookie: ", cookie); // => { name: 'vercel', value: 'fast', Path: '/' }
     // The outgoing response will have a `Set-Cookie:vercel=fast;path=/` header.
   }
-  return response
+  const allOutgoingCookies = response.cookies.getAll();
+  console.log("all outgoing cookies", allOutgoingCookies); // => [{ name: 'vercel', value: 'fast', path: '/' }]
+  return response;
 }
 
 export const config = {
-  matcher: '/demo',
-}
+  matcher: "/demo",
+};
